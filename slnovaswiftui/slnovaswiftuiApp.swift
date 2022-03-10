a86d48beb0edd9a900266aaa8fda7ba086e789c4
jcwang <jcwang0717@163.com>
2022-03-10 19:00:32 +0800

    Initial Commit

---
+++ slnovaswiftui/slnovaswiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  slnovaswiftuiApp.swift
+//  slnovaswiftui
+//
+//  Created by Jcwang on 2022/3/10.
+//
+
+import SwiftUI
+
+@main
+struct slnovaswiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
